fcc74414ab7f444fba92992871782f62cdf93888
afifasoft <abdul.pesit@gmail.com>
2020-03-22 12:03:40 +0530

    Expectations for Component Instances

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('shows a comment box', () => {
 
-  const div = document.createElement('div');
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 
-  ReactDOM.render(<App />, div);
-//  expect(div.innerHTML).toContain('Comment Box');
-//  expect(div.innerHTML).toBeTruthy();
-//  expect(div).toHaveAnInstanceOf(CommentBox);
-  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('shows a comment list', () => {
+
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 
 });
