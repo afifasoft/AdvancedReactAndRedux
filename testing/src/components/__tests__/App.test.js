e632d92c57d13e1ae32b06928336f275c8e1c6fa
afifasoft <abdul.pesit@gmail.com>
2020-03-22 11:13:08 +0530

    Enzyme setup

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -8,8 +8,9 @@ it('shows a comment box', () => {
   const div = document.createElement('div');
 
   ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain('Comment Box');
+//  expect(div.innerHTML).toContain('Comment Box');
 //  expect(div.innerHTML).toBeTruthy();
+//  expect(div).toHaveAnInstanceOf(CommentBox);
   ReactDOM.unmountComponentAtNode(div);
 
 });
