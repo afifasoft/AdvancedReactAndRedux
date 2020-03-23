24689930cd7cde09672d293baed0f2a350c36066
afifasoft <abdul.pesit@gmail.com>
2020-03-23 07:48:10 +0530

    TextArea Implementation

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Call an action creator
+    // And save the comment
+
+    this.setState({
+      comment: ''
+    });
+    
+  };
 
   render() {
 
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a Comment </h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment}/>
         <div>
           <button>Submit Comment</button>
         </div>
       </form>
     );
-    
+
   }
 }
 
