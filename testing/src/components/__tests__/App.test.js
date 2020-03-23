0448788114185bf241cd6b39eab8d842d53471ca
afifasoft <abdul.pesit@gmail.com>
2020-03-23 07:37:07 +0530

    Comment Box Component

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+// jest
+beforeEach(() => {
+   wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
 
-  const wrapped = shallow(<App />);
+//  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
 
 });
 
 it('shows a comment list', () => {
 
-  const wrapped = shallow(<App />);
+//  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
 
 });
--- testing/src/index.js
+++ testing/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
-// 19
+import App from 'components/App';
+// 24
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
