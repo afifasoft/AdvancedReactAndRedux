076682ef670ebb7b429212997c4e0c1db9a65d2b
afifasoft <abdul.pesit@gmail.com>
2020-03-24 08:42:10 +0530

    Retrieving Prop values

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -3,9 +3,27 @@ import { mount } from 'enzyme';
 
 import CommentBox from 'components/CommentBox';
 
-it('has a text area and a button', () => {
-  const wrapped = mount(<CommentBox />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has a text area and a button', () => {
+//  const  wrapped = mount(<CommentBox />);
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
+
+});
+
+it('has a textarea that users can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
--- testing/src/index.js
+++ testing/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from 'components/App';
-// 24
+// 38
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
