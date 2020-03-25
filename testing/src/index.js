0eb4ffc5d94d40ec34087080030a1aac1e707042
afifasoft <abdul.pesit@gmail.com>
2020-03-25 08:51:59 +0530

    Provider Tag

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from 'reducers';
 
 import App from 'components/App';
 // 38
 ReactDOM.render(
-  <App />,
+  // list of reducers and state 
+  <Provider store={createStore(reducers, {})}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
+
 );
