efb3e9017f3d5998f8322b4994fd07c278cd6a4f
afifasoft <abdul.pesit@gmail.com>
2020-03-25 07:29:10 +0530

    Describe Statements

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -20,10 +20,27 @@ it('has a text area and a button', () => {
 
 });
 
-it('has a textarea that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+
+describe('the text area', () => {
+
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
   });
-  wrapped.update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+
+  it('has a textarea that users can type in', () => {
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+
+  it('when form is submitted, text area gets emptied', () => {
+
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+
 });
