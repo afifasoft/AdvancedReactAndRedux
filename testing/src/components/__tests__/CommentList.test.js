cd07c8cfe1b44cb8f006e7ad3d0a6565efdf850b
afifasoft <abdul.pesit@gmail.com>
2020-03-29 09:16:58 +0530

    Getting Data into Redux

--- testing/src/components/__tests__/CommentList.test.js
+++ testing/src/components/__tests__/CommentList.test.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { mount } from 'enzyme';
 
+import CommentList from 'components/CommentList';
+import Root from 'Root';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <CommentList />
+    </Root>
+  );
+
+});
+
+it('creates one LI per comment', () => {
+
+});
