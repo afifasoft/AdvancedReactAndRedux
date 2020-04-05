5d1d501bf6eb33bdce68f06cf0e628158c4fd317
afifasoft <abdul.pesit@gmail.com>
2020-04-05 08:25:25 +0530

    Changing Auth State

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -3,17 +3,22 @@ import { Route, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-class App extends Component {
+class App extends Component { 
 
   renderButton() {
     if (this.props.auth) {
       return (
-        <button>Sign Out</button>
+        <button onClick={() => this.props.changeAuth(false)}>
+          Sign Out
+        </button>
       );
     } else {
       return (
-        <button>Sig In</button>
+        <button onClick={() => this.props.changeAuth(true)}>
+          Sig In
+        </button>
       );
     }
   }
@@ -50,4 +55,4 @@ function mapStateToProps(state) {
   return { auth: state.auth };
 }
 
-export default connect(mapStateToProps, {})(App);
+export default connect(mapStateToProps, actions)(App);
