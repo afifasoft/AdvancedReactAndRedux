87c1a331f8338308c06ba3a81f741550e0dc26b0
afifasoft <abdul.pesit@gmail.com>
2020-04-09 09:33:54 +0530

    State Validation Middleware

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
+//import reduxPromise from 'redux-promise';
+import async from 'middlewares/async';
 import reducers from 'reducers';
 
 //export default (props) => {
@@ -10,7 +11,7 @@ export default ({ children, initialState = {} }) =>{   // destructure props
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware()
+    applyMiddleware(async)
   );
 
   return (
--- testing/src/middlewares/async.js
+++ testing/src/middlewares/async.js
@@ -12,6 +12,7 @@ export default ({ dispatch }) => next => action => {
   // If it does, then wait for it to resolve
   // If it doesn't then send the action on to the
   // next middleware
+  
   if (!action.payload || !action.payload.then) {
     return next(action);
   }
